0a775293235b282b88578a89e6e644760eda1df0
Oma Withers <omacwitherssei@gmail.com>
2022-09-09 13:54:34 -0400

    "corrected heroku code"

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-
-require('dotenv').config()
-
 //___________________
 //Dependencies
 //___________________
@@ -8,6 +5,7 @@ const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require ('mongoose');
 const app = express();
+require('dotenv').config()
 const db = mongoose.connection;
 //___________________
 //Port
